a7e05825316537d55ae45b1eba4281045b6a5206
Leonardo <leonardoms.2010@hotmail.com>
2024-10-25 12:18:10 -0300

    It was removed the contact me button temporarily

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,4 +1,4 @@
-import Options from "../Body/Cogs/Options";
+// import Options from "../Body/Cogs/Options";
 import styles from "./NavBar.module.css";
 
 function NavBar() {
@@ -8,7 +8,7 @@ function NavBar() {
          <ul className="flex flex-row items-center">
           <button onClick={() => { window.open("https://github.com/LeoMallet04")}} className={styles.ButtonStyle} > <img className="h-9 w-9" src="/assets/navbar/github.png" alt="GitHub" /></button>
           <button onClick={() => { window.open("https://www.linkedin.com/in/leonardo-mallet-65a19931a/")}}className={styles.ButtonStyle}> <img className="h-9 w-9" src="/assets/navbar/linkedin.png" alt="Linkedin" /></button>
-          <Options></Options>
+          {/* <Options></Options> */}
          </ul>
         </nav>
     </header>
