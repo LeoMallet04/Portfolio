0134224297fda129362f8e80c9f2502623f5b594
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-05-25 22:02:03 -0300

    refact: Some adjusts in the FirstSection

--- src/components/Body/FirstScreen.tsx
+++ src/components/Body/FirstScreen.tsx
@@ -1,6 +1,7 @@
 function FirstSection(){
     return (
         <>
+        <div className="max-w-full h-auto">
         <section className="flex flex-row items-center justify-center pt-40">
             <div className="pr-72">
             <h1 className="text-9xl font-bold text-name-color">Leonardo
@@ -15,8 +16,10 @@ function FirstSection(){
             <h2 className="text-2xl pr-2 pl-5 font-semibold">I make software that solves </h2>
             <h2 className="text-2xl pr-36 pl-5 text-alternative-text-color font-semibold">your problems</h2>
             </div>
+            
         </section>
-        <img className="max-w-max h-auto" src="/assets/triang.png" alt="Triangle"/>
+        <img className="max-w-full h-auto" src="/assets/triang.png" alt="Triangle"/>
+        </div>
         </>
 
         
