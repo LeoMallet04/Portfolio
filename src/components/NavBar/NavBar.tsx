f7241da3138aeb94b4a71d59992ac8599f5cb946
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-05-25 21:33:57 -0300

    refact: Changes on the Linkedin link

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -7,7 +7,7 @@ function NavBar() {
       <nav className={styles.NavBarContent}>
          <ul className="flex flex-row items-center">
           <button onClick={() => { window.open("https://github.com/LeoMallet04")}} className={styles.ButtonStyle} > <img className="h-9 w-9" src="/assets/navbar/github.png" alt="GitHub" /></button>
-          <button onClick={() => { window.open("https://www.linkedin.com/in/leonardo-mallet-65a19931a/")}}className={styles.ButtonStyle}> <img className="h-9 w-9" src="/assets/navbar/linkedin.png" alt="Linkedin" /></button>
+          <button onClick={() => { window.open("https://www.linkedin.com/in/leonardo-scheidt/")}}className={styles.ButtonStyle}> <img className="h-9 w-9" src="/assets/navbar/linkedin.png" alt="Linkedin" /></button>
           {/* <Options></Options> */}
          </ul>
         </nav>
