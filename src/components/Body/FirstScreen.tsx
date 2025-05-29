b482b11d22b96c3090fd5d333c2aca6351ad8373
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-05-28 21:40:14 -0300

    refact: Change the FirstScreen static text to a more dynamic via props

--- src/components/Body/FirstScreen.tsx
+++ src/components/Body/FirstScreen.tsx
@@ -1,20 +1,20 @@
-function FirstSection(){
+function FirstSection(props:{firstName: string, secondName: string, firstSubtittle: string, secondSubtittle: string}){
     return (
         <>
         <div className="max-w-full h-auto">
         <section className="flex flex-row items-center justify-center pt-40">
             <div className="pr-72">
-            <h1 className="text-9xl font-bold text-name-color">Leonardo
+            <h1 className="text-9xl font-bold text-name-color">{props.firstName}
             </h1>
-            <h1 className="text-9xl font-bold pr-28 ">Scheidt</h1>
+            <h1 className="text-9xl font-bold pr-28 ">{props.secondName}</h1>
             {/* <h1 className="text-xl font-bold text-gray-300 opacity-60 pr-80 pt-5">Front-End Developer</h1> */}
             </div>
             
             <div>
             {/* Faço softwares que resolvem
             problemas reais */}
-            <h2 className="text-2xl pr-2 pl-5 font-semibold">I make software that solves </h2>
-            <h2 className="text-2xl pr-36 pl-5 text-alternative-text-color font-semibold">your problems</h2>
+            <h2 className="text-2xl pr-2 pl-5 font-semibold">{props.firstSubtittle}</h2>
+            <h2 className="text-2xl pr-36 pl-5 text-alternative-text-color font-semibold">{props.secondSubtittle}</h2>
             </div>
             
         </section>
