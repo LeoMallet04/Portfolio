4adda131b8a7b60718e4eb39723f2596f2430633
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-05-28 21:35:38 -0300

    fact: Creation of an enum that contains all the visual texts of the application in two versions, one in Brazilian Portuguese and the other in English

---
+++ src/components/Body/Cogs/DescriptionsEnum.tsx
@@ -0,0 +1,43 @@
+
+enum descriptionsENG{
+    ABOUTME = "I am currently in the fourth semester of the Software Engineering program at PUCRS. Throughout this period, I have honed my programming skills and worked on several projects. Furthermore, I am committed to continuous personal growth and expanding my knowledge in programming daily. In this website i would be delighted to share some of my work with you.",
+    HTML = "Markup language used to structure and present content on the web. Defines elements such as text, images, and links.",
+    CSS = "Style sheets used to style the layout of HTML pages, controlling colors, fonts, and arrangement of elements.",
+    JAVA = "Object-oriented programming language, widely used for backend and Android application development.",
+    JAVASCRIPT = "Web scripting language, allowing you to create interactivity and manipulate the DOM in HTML pages.",
+    TYPESCRIPT = "A superset of JavaScript that adds static typing, making code more robust and easier to maintain.",
+    GIT = "Distributed version control system to track project changes and collaborate efficiently.",
+    REACT = "JavaScript library for building component-based user interfaces and efficient state management.",
+    TAILWIND= "CSS framework utility that makes it easy to create custom layouts directly in HTML.",
+    FLUTTER = "Flutter is an open-source framework from Google for developing cross-platform applications, allowing you to create native interfaces for Android, iOS, web and desktop.",   
+    NODEJS = "Backend JavaScript environment that allows code execution on the server. Ideal for APIs and real-time applications.",
+    PRISMA = "ORM (Object-Relational Mapping) that facilitates interaction with databases, automating complex queries.",
+    MONGODB = "Document-oriented NoSQL database suitable for applications with large volumes of unstructured data.",
+    FIGMA = "Collaborative design tool for creating interactive interfaces and prototypes.",
+    DOCKER = "Container platform that facilitates the creation, distribution and execution of applications in isolated environments.",
+    POSTMAN = "Tool for testing and documenting APIs, facilitating development and integration between services.",
+    SPRINGBOOT = "Java framework that simplifies the development of backend applications with a focus on productivity and good practices.",
+   
+}
+
+enum descriptionsBR {
+    ABOUTME = "Atualmente estou cursando o 4º semestre de Engenharia de Software na PUCRS, nesse tempo de formação desenvolvi bons conhecimentos de programação e alguns projetos, os quais irei compartilhar nesse site. Além disso, adoro buscar diariamente evoluir como pessoa e aumentar meus conhecimentos sobre programação. Venho mostrar um pouco do meu trabalho, sempre procurando resolver problemas reais por meio da programação",
+    HTML = "Linguagem de marcação usada para estruturar e apresentar conteúdo na web. Define elementos como textos, imagens e links.",
+    CSS = "Folhas de estilo usadas para estilizar o layout de páginas HTML, controlando cores, fontes, e disposição de elementos.",
+    JAVA = "Linguagem de programação orientada a objetos, amplamente usada para desenvolvimento de aplicações backend e Android.",
+    JAVASCRIPT = "Linguagem de script para web, permitindo criar interatividade e manipular o DOM em páginas HTML.",
+    TYPESCRIPT = "Superconjunto do JavaScript que adiciona tipagem estática, tornando o código mais robusto e fácil de manter.",
+    GIT = "Sistema de controle de versão distribuído para rastrear alterações em projetos e colaborar de forma eficiente.",
+    REACT = "Biblioteca JavaScript para construção de interfaces de usuário, baseada em componentes e gerenciamento eficiente de estado.",
+    TAILWIND = "Framework CSS utilitário que facilita a criação de layouts personalizados diretamente no HTML.",
+    FLUTTER = "Flutter é um framework open-source do Google para desenvolvimento de aplicativos multiplataforma, permitindo criar interfaces nativas para Android, iOS, web e desktop.",
+    NODEJS = "Ambiente JavaScript de backend que permite a execução de código no servidor. Ideal para APIs e aplicações em tempo real.",
+    PRISMA = "ORM (Object-Relational Mapping) que facilita a interação com bancos de dados, automatizando queries complexas.",
+    MONGODB = "Banco de dados NoSQL orientado a documentos, adequado para aplicações com grandes volumes de dados não estruturados.",
+    FIGMA = "Ferramenta de design colaborativo para criação de interfaces e protótipos interativos.",
+    DOCKER = "Plataforma de contêineres que facilita a criação, distribuição e execução de aplicações em ambientes isolados.",
+    POSTMAN = "Ferramenta para testar e documentar APIs, facilitando o desenvolvimento e a integração entre serviços.",
+    SPRINGBOOT = "Framework Java que simplifica o desenvolvimento de aplicações backend com foco em produtividade e boas práticas.",
+}
+
+export {descriptionsENG, descriptionsBR};
