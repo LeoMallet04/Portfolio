2ee4312fa8f8a9a3dcc3ac6df8fc1f1f03e6cff8
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-05-28 21:37:10 -0300

    fact; Creation of two buttons that changes the language

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,11 +1,15 @@
 // import Options from "../Body/Cogs/Options";
 import styles from "./NavBar.module.css";
 
-function NavBar() {
+
+function NavBar({changeLanguage}: {changeLanguage: (language:string) => void}) {
   return (
     <header className={styles.NavBarBody}>
       <nav className={styles.NavBarContent}>
          <ul className="flex flex-row items-center">
+          <button onClick={() => changeLanguage("BR")} className="h-10 w-10 bg-black"></button>
+          <button onClick={() => changeLanguage("ENG")} className="h-10 w-10 bg-black"></button>
+          <div className=""></div>
           <button onClick={() => { window.open("https://github.com/LeoMallet04")}} className={styles.ButtonStyle} > <img className="h-9 w-9" src="/assets/navbar/github.png" alt="GitHub" /></button>
           <button onClick={() => { window.open("https://www.linkedin.com/in/leonardo-scheidt/")}}className={styles.ButtonStyle}> <img className="h-9 w-9" src="/assets/navbar/linkedin.png" alt="Linkedin" /></button>
           {/* <Options></Options> */}
