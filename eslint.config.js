88cb34ea5f4addb5cd0c8d75e16757b596b87362
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-09-18 13:07:35 -0300

    chore: Reverted "upgrade"

--- eslint.config.js
+++ eslint.config.js
@@ -1,59 +1,28 @@
 import js from '@eslint/js'
 import globals from 'globals'
-import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
-import jsxA11y from 'eslint-plugin-jsx-a11y'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist', 'node_modules'] },
+  { ignores: ['dist'] },
   {
-    files: ['**/*.{ts,tsx,js,jsx}'],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      sourceType: 'module',
       globals: globals.browser,
     },
     plugins: {
-      react,
       'react-hooks': reactHooks,
-      'jsx-a11y': jsxA11y,
       'react-refresh': reactRefresh,
-      '@typescript-eslint': tseslint,
     },
-    extends: [
-      js.configs.recommended,
-      react.configs.recommended,
-      reactHooks.configs.recommended,
-      jsxA11y.configs.recommended,
-      tseslint.configs.recommended,
-    ],
     rules: {
-      // React
-      'react/react-in-jsx-scope': 'off',
-      'react/prop-types': 'off',
-
-      // TypeScript
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
-      ],
-
-      // Hooks
       ...reactHooks.configs.recommended.rules,
-
-      // Accessibility
-      ...jsxA11y.configs.recommended.rules,
-
-      // React Refresh
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
     },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-  }
+  },
 )
