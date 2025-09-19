2e08e0a238227663760e9de54f59e69d2a8341b6
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-09-19 13:43:49 -0300

    chore: Qlty upgraded with new plugins

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,57 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ["dist", "node_modules"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      React,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      "jsx-a11y": jsxA11y,
+      "@typescript-eslint": tseslint,
     },
+    extends: [
+      js.configs.recommended,
+      React.configs.recommended,
+      reactHooks.configs.recommended,
+      jsxA11y.configs.recommended,
+      tseslint.configs.recommended,
+    ],
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
+      // React
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
+
+      // TypeScript
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+
+      ...reactHooks.configs.recommended.rules,
+
+      ...jsxA11y.configs.recommended.rules,
+  
     },
-  },
-)
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  }
+);
