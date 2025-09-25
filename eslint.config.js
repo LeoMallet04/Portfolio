2e1d89394df6254b7ebb14fe151954b35b0b780a
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-09-25 11:54:04 -0300

    chore: Ajustes no eslint.config para passar na nuvem

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,30 @@
 import js from "@eslint/js";
 import globals from "globals";
+import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
-export default tseslint.config(
-  { ignores: ["dist", "node_modules"] },
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
+    ignores: ["dist", "node_modules"],
+  },
+  {
+    files: ["**/*.{ts,tsx,js,jsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parser: tsParser,
     },
     plugins: {
-      React,
+      react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
       "jsx-a11y": jsxA11y,
       "@typescript-eslint": tseslint,
     },
-    extends: [
-      js.configs.recommended,
-      React.configs.recommended,
-      reactHooks.configs.recommended,
-      jsxA11y.configs.recommended,
-      tseslint.configs.recommended,
-    ],
     rules: {
       // React
       "react/react-in-jsx-scope": "off",
@@ -43,15 +41,21 @@ export default tseslint.config(
         { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
       ],
 
+      // Recomendações extras dos plugins
       ...reactHooks.configs.recommended.rules,
-
       ...jsxA11y.configs.recommended.rules,
-  
     },
     settings: {
       react: {
-        version: 'detect',
+        version: "detect",
       },
     },
-  }
-);
+    extends: [
+      js.configs.recommended,
+      react.configs.recommended,
+      reactHooks.configs.recommended,
+      jsxA11y.configs.recommended,
+      tseslint.configs.recommended,
+    ],
+  },
+];
